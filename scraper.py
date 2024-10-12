a0c3c702f4c1459238d459f55aaa8aa2f979334f
cecepizza <pizzacort@iivi.co>
2024-10-12 14:56:24 -0400

    webscraping colorhunt.co

---
+++ scraper.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+# setup basic structure to fetch color hunt website -- 
+def scraper_color():
+    url= "https://colorhunt.co/"
+    response = requests.get(url)
+
+# create a BeautifulSoup object to parse through the site 
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # find all the color palette elements 
+        palette_elements = soup.find_all('div', class_='item')
+
+        for palette in palette_elements[:5]: # loop through first five palettes
+        # extract color codes 
+            color_divs = palette.find_all('div', class_='palette') # within each palette find each individual color 
+        # loop through color_divs and create list of color codes for the palette 
+            color_codes = [div['data-copy'] for div in color_divs] # extract the color codes from 'data-copy' attribute of each color div 
+        # need to inspect elements of the dev tools to get exact name of HEX variable 
+
+        # # extract likes count 
+        # # find <span> extract its contents 
+        #     likes = palette.find('span', class_='palette-likes').text.strip()
+
+        # find and extract color data - print 
+            print("successfully fetched colors")
+            print(f"Colors: {', '.join(color_codes)}")
+            # print(f"Likes: {likes}")
+            print("---")
+
+    else:
+            print(f"no color soup for you status: {response.status_code}")
+
+if __name__ == "__main__":
+    scraper_color()
